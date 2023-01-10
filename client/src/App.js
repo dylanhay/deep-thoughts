bc1eb9628cbabdb5b3e180e4dec43e6394100fc4
dchay <dylanhay@rogers.com>
2023-01-10 12:01:50 -0500

    user-base mutations implemented on client side

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ function App() {
                 <Route path="" element={<Profile />} />
               </Route>
               <Route path="/thought/:id" element={<SingleThought />} />
-
+              <Route path="/signup" element={<Signup />} />
               <Route path="*" element={<NoMatch />} />
             </Routes>
           </div>
